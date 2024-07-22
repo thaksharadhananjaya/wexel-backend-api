658d06dffa9c6e044895843720cf2d1e4b574287
Thakshara Dhananjaya <thakshara@surge.global>
2024-07-23 02:40:40 +0530

    feat: crate global error handler

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import { Routes } from "./routes";
 import ENVIRONMENT from "./config/environment";
+import globalErrorHandler from "./middleware/global-error-handler";
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/v1/", Routes.routes);
+// Global error handling middleware
+app.use(globalErrorHandler);
 
 const { HOST, PORT } = ENVIRONMENT;
 
---
+++ src/middleware/global-error-handler.ts
@@ -0,0 +1,20 @@
+// src/middleware/globalErrorHandler.ts
+import { Request, Response, NextFunction } from "express";
+import { BaseException } from "../exception/base-exception";
+
+const globalErrorHandler = (
+  err: BaseException,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { status, statusCode, message } = err;
+
+  res.status(err.statusCode).json({
+    status,
+    statusCode,
+    error: { message },
+  });
+};
+
+export default globalErrorHandler;
