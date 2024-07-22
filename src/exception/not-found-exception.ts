bb1b50096bf35e74093bb9a4f206c58ac2559fd5
Thakshara Dhananjaya <thakshara@surge.global>
2024-07-23 02:40:19 +0530

    create exception

---
+++ src/exception/not-found-exception.ts
@@ -0,0 +1,7 @@
+import { BaseException } from "./base-exception";
+
+export class NotFoundException extends BaseException {
+  constructor(message: string) {
+    super(message, "NOT-FOUND", 404);
+  }
+}
