c53c31cdbe951bfaf5197b6979a2d34c7eb4283c
Thakshara Dhananjaya <thakshara@surge.global>
2024-08-07 23:45:54 +0530

    Add Cors

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,24 @@
+import { RegisterRoutes } from '../build/generated/routes';
+import * as swaggerJson from '../build/generated/swagger.json';
 import ENVIRONMENT from './config/environment';
 import globalErrorHandler from './middlewares/global-error-handler.middleware';
 import { httpResponse } from './middlewares/http-response.middleware';
-import { RegisterRoutes } from '../build/generated/routes';
+import cors from 'cors';
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
-import * as swaggerJson from "../build/generated/swagger.json";
 
 const app = express();
 
 app.use(express.json());
 app.use(httpResponse);
-app.use(
-    '/api/docs',
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerJson)
-);
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerJson));
 
 RegisterRoutes(app);
 // Global error handling middleware
 app.use(globalErrorHandler);
+
+// Use CORS middleware
+app.use(cors());
 
 const { HOST, PORT } = ENVIRONMENT;
 
